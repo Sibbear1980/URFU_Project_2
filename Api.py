c21279f9240ea73d5ba1bbda2763cfb826bf925f
Sibbear1980 <ds.shabanov@yandex.ru>
2024-02-26 23:40:55 +0300

    Загружаем тестовый скрипт

--- Api.py
+++ Api.py
@@ -1,21 +1,17 @@
-# определение эмоционального окраса текста
-
-# импортируем необходимые библиотеки
-
 from fastapi import FastAPI
 import torch
-
 from transformers import AutoModelForSequenceClassification
 from transformers import BertTokenizerFast
 
-app = FastAPI()
 
+app = FastAPI()
 tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment')
 model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment', return_dict=True)
 result = ['нейтральный', 'позитивный :)', 'негативный ((']
 
+
 @app.post("/predict/")       # адрес для вызова модели методом post 
-def predict(input:dict):     # функция для расчета модели, в "input" передаются текст для определения эмоционального окраса
+def predict(input: dict):     # функция для расчета модели, в "input" передаются текст
     inputs = tokenizer(input['text'], max_length=512, padding=True, truncation=True, return_tensors='pt')
     outputs = model(**inputs)
     predicted = torch.nn.functional.softmax(outputs.logits, dim=1)
