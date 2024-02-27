96c986d14509190836990c759cab091e9b7438d2
KateProxa <ekater_1997@mail.ru>
2024-02-27 23:17:15 +0300

    Правки в коде по PEP8

--- Api.py
+++ Api.py
@@ -4,16 +4,32 @@ from transformers import AutoModelForSequenceClassification
 from transformers import BertTokenizerFast
 
 app = FastAPI()
-tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment')
-model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment', return_dict=True)
-result = ['нейтральный', 'позитивный :)', 'негативный ((']
+tokenizer = BertTokenizerFast.from_pretrained(
+    "blanchefort/rubert-base-cased-sentiment"
+    )
+model = AutoModelForSequenceClassification.from_pretrained(
+    "blanchefort/rubert-base-cased-sentiment", return_dict=True
+)
+result = ["нейтральный", "позитивный :)", "негативный (("]
 
 
-@app.post("/predict/")       # адрес для вызова модели методом post 
-def predict(input: dict):     # функция для расчета модели, в "input" передаются текст
-    inputs = tokenizer(input['text'], max_length=512, padding=True, truncation=True, return_tensors='pt')
+@app.post("/predict/")  # адрес для вызова модели методом post
+# функция для расчета модели
+# в "input" передаётся текст
+def predict(input: dict):
+    inputs = tokenizer(
+        input["text"],
+        max_length=512,
+        padding=True,
+        truncation=True,
+        return_tensors="pt",
+    )
     outputs = model(**inputs)
     predicted = torch.nn.functional.softmax(outputs.logits, dim=1)
     predicted = torch.argmax(predicted, dim=1).numpy()
-    output = {'Исходный текст': input['text'], 'Результат:': result[int(predicted[0])]}  # формируем результат
+    # формируем результат
+    output = {
+        "Исходный текст": input["text"],
+        "Результат:": result[int(predicted[0])]
+        }
     return output
--- Api_test.py
+++ Api_test.py
@@ -1,36 +1,46 @@
-# тестирование модели эмоц. окраса текста из задания 3 https://github.com/Vika00224/URFU_Project_1/tree/main/HomeWork%203%20All
+"""
+тестирование модели
+эмоционального окраса текста
+"""
 
 from fastapi.testclient import TestClient
-from Api import app                        # импорт приложения из App.py
+
+# импорт приложения из App.py
+from Api import app
 
 
 client = TestClient(app)
 
 
 # тест 1
-def test_predict():
-    response = client.post("/predict/",
-        json={"text": "It was a wonderful journey. We have visited many beautiful places and seen many sightings! I am happy!"}
+def test_predict_1():
+    response = client.post(
+        "/predict/",
+        json={
+            "text":
+            """It was a wonderful journey.
+            We have visited many beautiful places
+            and seen many sightings! I am happy!"""
+        },
     )
-    json_data = response.json() 
+    json_data = response.json()
+    # проверка ответа приложения
+    assert response.status_code == 200
+    # проверка правильности работы модели определения эмоц окраса текста
+    assert json_data["Результат:"] == "позитивный :)"
 
-    assert response.status_code == 200                 # проверка ответа приложения
-    assert json_data['Результат:'] == 'позитивный :)'  # проверка правильности работы модели определения эмоц окраса текста
 
 # тест 2
-def test_predict():
-    response = client.post("/predict/",
-        json={"text": "I hate traffic jams"}
-    )
-    json_data = response.json() 
+def test_predict_2():
+    response = client.post("/predict/", json={"text": "I hate traffic jams"})
+    json_data = response.json()
 
-    assert json_data['Результат:'] == 'негативный (('
+    assert json_data["Результат:"] == "негативный (("
+
 
 # тест 3
-def test_predict():
-    response = client.post("/predict/",
-        json={"text": "Today spring has come"}
-    )
-    json_data = response.json() 
+def test_predict_3():
+    response = client.post("/predict/", json={"text": "Today spring has come"})
+    json_data = response.json()
 
-    assert json_data['Результат:'] == 'нейтральный'    
+    assert json_data["Результат:"] == "нейтральный"
